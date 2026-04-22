b584eae5c03530645d0028f022458e4c9552f876
Sarvesh Kumar <abcode1024@gmail.com>
2026-04-22 17:56:23 +0530

    bcrypt add

--- index.mjs
+++ index.mjs
@@ -1,22 +1,24 @@
 import express from 'express'
 import {prisma} from "./Prisma/prisma_client.mjs"
+import bcrypt from 'bcrypt';
 const app = express()
 const port = 5000
 
 app.use(express.json())
 
 
-// app.post("/signup", async (req,res)=>{
-//     console.log(req.body)
-//     const user = await prisma.user.create({
-//         data:{
-//             email:req.body.email,
-//             name:req.body.name,
-//             password:req.body.password
-//         }
-//     })
-//     res.json({user})
-// })
+app.post("/signup", async (req,res)=>{
+    console.log(req.body)
+    const hashedpassword = await bcrypt.hash(req.body.password,10)
+    const user = await prisma.user.create({
+        data:{
+            email:req.body.email,
+            name:req.body.name,
+            password:hashedpassword
+        }
+    })
+    res.json({user})
+})
 
 app.post("/login",async (req,res)=>{
     const user=  await prisma.user.findUnique({
@@ -30,13 +32,13 @@ app.post("/login",async (req,res)=>{
         })
         return
     }
-    if(user.password !== req.body.password){
+    if(!await bcrypt.compare(req.body.password,user.password)){
         res.status(401).json({
             "error":"password not found"
         })
         return
     }
-    res.json({massage:"login successfull"})
+    res.json({massage:`login successfull welcome ${user.name}`})
 })
 
 
